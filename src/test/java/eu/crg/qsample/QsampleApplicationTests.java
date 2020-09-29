632ddd52275939fe599fce1e1fd973f7e47a0050
Vesperon51 <vesperon51@gmail.com>
2020-09-29 14:08:44 +0200

    UnitTests File

--- src/test/java/eu/crg/qsample/QsampleApplicationTests.java
+++ src/test/java/eu/crg/qsample/QsampleApplicationTests.java
@@ -2,8 +2,10 @@ package eu.crg.qsample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("unittest")
 class QsampleApplicationTests {
 
 	@Test
