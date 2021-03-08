57a8ea9102f7e28c690b870184d57d10b7d3cd11
Marc Serret <vesperon51@gmail.com>
2021-03-08 13:38:05 +0100

    Check only protein only one time per sample quantification

--- src/main/java/eu/crg/qsample/quantification/QuantificationRepository.java
+++ src/main/java/eu/crg/qsample/quantification/QuantificationRepository.java
@@ -1,11 +1,14 @@
 package eu.crg.qsample.quantification;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface QuantificationRepository extends CrudRepository<Quantification, Long> {
 
     List<Quantification>findFirst5ByFileChecksumAndContaminantOrderByAbundanceDesc(String checksum, boolean contaminant);
+
+    Optional<Quantification> findByFileChecksumAndAccession(String checksum, String accession);
 
 }
