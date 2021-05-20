08a60230cfc4f51538e4a626f5edba48d7d2906c
Marc Serret <vesperon51@gmail.com>
2021-05-20 11:30:40 +0200

    Modificartions entry points

---
+++ src/main/java/eu/crg/qsample/modification/ModificationService.java
@@ -0,0 +1,44 @@
+package eu.crg.qsample.modification;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.stereotype.Service;
+
+import eu.crg.qsample.file.RequestFile;
+import eu.crg.qsample.file.RequestFileRepository;
+
+@Service
+public class ModificationService {
+
+    @Autowired
+    RequestFileRepository fileRepo;
+
+    @Autowired
+    ModificationRepository modRepo;
+
+    @Autowired
+    ModificationFileRepository modFileRepo;
+
+    public void insertModificationsFromPipeline(ModificationFromPipeline modificationFromPipeline) {
+        Optional <RequestFile> rFileOpt = fileRepo.findOneByChecksum(modificationFromPipeline.getFile().getChecksum());
+        if (!rFileOpt.isPresent()) {
+            System.out.println("File not found");
+            throw new DataRetrievalFailureException("File not found");
+        }
+        for (ModificationFile mod: modificationFromPipeline.getData()) {
+            Optional <Modification> modOpt = modRepo.findOneByName(mod.getModification().getName());
+            ModificationFile modFile = new ModificationFile();
+            if (modOpt.isPresent()) { // mod already at BD, use existing entity
+                modFile.setModification(modOpt.get());
+            } else { // mod not at DB, save and use it
+                Modification saved = modRepo.save(mod.getModification());
+                modFile.setModification(saved);
+            }
+            modFile.setFile(rFileOpt.get());
+            modFile.setValue(mod.getValue());
+            modFileRepo.save(modFile);
+        }
+    }
+}
