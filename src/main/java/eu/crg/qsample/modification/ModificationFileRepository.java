ef2a096daec9e54c4a62d90c0ba055b03673341f
Marc Serret <vesperon51@gmail.com>
2021-06-08 12:09:51 +0200

    Insert mod checks

--- src/main/java/eu/crg/qsample/modification/ModificationFileRepository.java
+++ src/main/java/eu/crg/qsample/modification/ModificationFileRepository.java
@@ -1,7 +1,13 @@
 package eu.crg.qsample.modification;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
+import eu.crg.qsample.file.RequestFile;
+
 public interface ModificationFileRepository extends CrudRepository<ModificationFile, Long> {
+
+    public Optional <ModificationFile> findOneByFileAndModification(RequestFile file, Modification mod);
 
 }
