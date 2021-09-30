8b007b56b1a0d9289ec6d47599d7e21bb400cf3d
Marc Serret <vesperon51@gmail.com>
2021-09-30 12:28:25 +0200

    create new local request and delete it

--- src/main/java/eu/crg/qsample/request/RequestController.java
+++ src/main/java/eu/crg/qsample/request/RequestController.java
@@ -1,21 +1,29 @@
 package eu.crg.qsample.request;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import eu.crg.qsample.exceptions.NotFoundException;
 import eu.crg.qsample.request.local.RequestLocal;
 
 @RestController
@@ -51,6 +59,18 @@ public class RequestController {
         return requestService.getLocalRequestById(id);
     }
 
+    @PreAuthorize("hasRole('MANAGER')")
+    @PostMapping(value = "saveLocal")
+    public RequestLocal saveLocal(@RequestBody RequestLocal localRequest) {
+        return requestService.saveLocal(localRequest);
+    }
+
+    @PreAuthorize("hasRole('MANAGER')")
+    @DeleteMapping(value = "deleteLocal/{id}")
+    public boolean deleteLocal(@PathVariable(name = "id") Long localRequestId) {
+        return requestService.deleteLocal(localRequestId);
+    }
+
     @PreAuthorize("hasRole('USER')")
     @RequestMapping(value = "getPlotName/{csId}/{paramId}")
     public String getPlotName(@PathVariable Long csId, @PathVariable Long paramId) {
@@ -61,5 +81,10 @@ public class RequestController {
     @RequestMapping(value = "isQCloud2FilesEnabled")
     public boolean isQCloud2FilesEnabled() {
         return requestService.isQCloud2FilesEnabled();
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    void handleNotFoundException(HttpServletResponse response, Exception e) throws IOException {
+        response.sendError(HttpStatus.NOT_FOUND.value(), "Nothing found with this parameters");
     }
 }
