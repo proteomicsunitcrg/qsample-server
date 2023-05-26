10a39f95e527ffa041b33188e27ed34501ff4657
Toni Hermoso Pulido <toniher@cau.cat>
2023-05-26 13:03:21 +0200

    moving binary to char

--- src/main/java/eu/crg/qsample/threshold/Threshold.java
+++ src/main/java/eu/crg/qsample/threshold/Threshold.java
@@ -33,8 +33,9 @@ public class Threshold {
     @SequenceGenerator(name = "threshold_seq", sequenceName = "threshold_seq", allocationSize = 1)
     protected Long id;
 
-    @Column(name = "apiKey", updatable = true, nullable = false, unique = true, columnDefinition = "BINARY(16)")
-    @org.hibernate.annotations.Type(type = "org.hibernate.type.UUIDBinaryType")
+    @Column(name = "apiKey", updatable = true, nullable = false, unique = true, columnDefinition = "CHAR(32)")
+    //@org.hibernate.annotations.Type(type = "org.hibernate.type.UUIDBinaryType")
+    @org.hibernate.annotations.Type(type="uuid-char")
     private UUID apiKey;
 
     @Enumerated(EnumType.STRING)
