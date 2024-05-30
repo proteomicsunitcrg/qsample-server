3ce5867ba41072ccdd8b75cc01d2b880e6b5d60d
Toni Hermoso Pulido <toni.hermoso@crg.eu>
2024-05-30 16:58:10 +0200

    sql for handling https://github.com/proteomicsunitcrg/atlas/issues/236

---
+++ src/main/resources/db/migration/V1_4__202405.sql
@@ -0,0 +1,2 @@
+update `context_source` set abbreviated="Total number of phospho sites", name="Total number of phospho sites" where id=24;
+insert ignore into `context_source` (id, abbreviated, api_key, charge, mz, name, trace_color_id) values (25,"Total number of phospho peptides","e01c53fd-7f11-4513-8e0b-2be6bcee11be",NULL,NULL,"Total number of phospho peptides",1);
