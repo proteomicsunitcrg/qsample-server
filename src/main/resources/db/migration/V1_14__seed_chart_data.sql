633abe331c59af5b329c209b6c89444095d7b25b
Roger Olivella <roger.olivella@crg.eu>
2026-05-15 12:08:42 +0200

    Seed initial chart configuration

---
+++ src/main/resources/db/migration/V1_14__seed_chart_data.sql
@@ -0,0 +1,33 @@
+INSERT INTO chart_definitions
+(name, title, description, chart_type, library, data_source_key, is_active)
+VALUES
+(
+  'request_status_chart',
+  'Request Status',
+  'Request status distribution',
+  'pie',
+  'plotly',
+  'request_status',
+  TRUE
+);
+
+INSERT INTO chart_parameters
+(chart_id, param_key, param_value, param_type, description)
+VALUES
+(
+  (SELECT id FROM chart_definitions WHERE name = 'request_status_chart'),
+  'height',
+  '400',
+  'number',
+  'Chart height'
+);
+
+INSERT INTO chart_page_assignments
+(chart_id, page_name, display_order, is_visible)
+VALUES
+(
+  (SELECT id FROM chart_definitions WHERE name = 'request_status_chart'),
+  'request_details',
+  1,
+  TRUE
+);
